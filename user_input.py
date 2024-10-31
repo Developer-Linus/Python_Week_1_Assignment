6b066869224ed6b819dabc433cb03f7fe5c9ad05
Developer-Linus <linusmksu30@gmail.com>
2024-10-31 20:00:55 +0300

    assignment completed based on given instructions.

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# We use the input() function to collect data/information from user.
+#Getting user name
+name = input('What is your name?')
+#Getting user age
+age = input('What is your current age in years?')
+# Getting user's location
+location = input('Where do you currenty live?')
+
+#Printing personalized message
+print(f'Hello {name}, you are {age} years old and live in {location}.' )
